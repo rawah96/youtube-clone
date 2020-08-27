b469109dd4e891e5282163f62912858523df8f39
rawah <rawah@pdx.edu>
2020-08-27 14:52:21 -0700

    Updates

---
+++ precache-manifest.d7c179018e560c37cc8f2f38bc56554a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b511eb2b2bc23cea3d7987f3ba4ef4d9",
+    "url": "/youtube-clone/index.html"
+  },
+  {
+    "revision": "686d0a4d6f2b99e2869a",
+    "url": "/youtube-clone/static/css/main.bc604602.chunk.css"
+  },
+  {
+    "revision": "b446f9983c860bc05236",
+    "url": "/youtube-clone/static/js/2.7fd5b4ad.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/youtube-clone/static/js/2.7fd5b4ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "686d0a4d6f2b99e2869a",
+    "url": "/youtube-clone/static/js/main.1e65a647.chunk.js"
+  },
+  {
+    "revision": "8a09cd451cb85b60110f",
+    "url": "/youtube-clone/static/js/runtime-main.a270c926.js"
+  },
+  {
+    "revision": "87bed15c938de2e6068d753ccc389f24",
+    "url": "/youtube-clone/static/media/800px-YouTube_Logo_2017.svg.87bed15c.png"
+  }
+]);
